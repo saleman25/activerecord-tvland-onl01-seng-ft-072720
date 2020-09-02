5a3cbf515b63dcadfc785deac7d09f7325d258f6
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-02 22:44:57 +0000

    Automatically backed up by Learn

--- db/migrate/20200902224340_add_day_and_genre_and_season_to_shows.rb.rb
+++ db/migrate/20200902224340_add_day_and_genre_and_season_to_shows.rb.rb
@@ -1,4 +1,4 @@
-class AddDayAndGenreAndSeasonToShows.rb < ActiveRecord::Migration[5.2]
+class AddDayAndGenreAndSeasonToShows < ActiveRecord::Migration[5.2]
   def change
     add_column :shows, :day, :string
     add_column :shows, :genre, :string
